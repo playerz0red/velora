1db9236632399f3191650c43d940c32f21503f6b
pavel.bychkouski <ipasha1337@yandex.by>
2026-07-01 11:49:54 +0300

    feat(setup): configure project

---
+++ velora/Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 6.1
+// This is a Skip (https://skip.dev) package.
+import PackageDescription
+
+let package = Package(
+    name: "velora",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v17), .macOS(.v14)],
+    products: [
+        .library(name: "VeloraApp", type: .dynamic, targets: ["VeloraApp"]),
+    ],
+    dependencies: [
+        .package(url: "https://source.skip.tools/skip.git", from: "1.9.4"),
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "VeloraApp", dependencies: [
+            .product(name: "SkipUI", package: "skip-ui")
+        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
+        .testTarget(name: "VeloraAppTests", dependencies: [
+            "VeloraApp",
+            .product(name: "SkipTest", package: "skip")
+        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
+    ]
+)
