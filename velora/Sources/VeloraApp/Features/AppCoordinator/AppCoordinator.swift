64b20bff66526881bca0ab4685769112d1c21d00
Angelina <ang.g02@mail.ru>
2026-07-06 10:58:44 +0300

    refactor(coordinator): move destination mapping to coordinator

--- velora/Sources/VeloraApp/Features/AppCoordinator/AppCoordinator.swift
+++ velora/Sources/VeloraApp/Features/AppCoordinator/AppCoordinator.swift
@@ -3,6 +3,7 @@ import SwiftUI
 @Observable
 
 final class AppCoordinator {
+    
     var path = NavigationPath()
     
     func push (_ route: AppRoute) {
@@ -16,5 +17,24 @@ final class AppCoordinator {
     
     func popToRoot() {
         path = NavigationPath()
+    }
+}
+
+extension AppCoordinator {
+    @ViewBuilder
+    func destination(for route: AppRoute) -> some View {
+        switch route {
+        case .auth:
+            AuthView()
+            
+        case .email:
+            EmailView()
+            
+        case .registration:
+            Text("Registration")
+            
+        case .home:
+            Text("Home")
+        }
     }
 }
